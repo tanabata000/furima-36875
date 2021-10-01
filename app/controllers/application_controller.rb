a8e33e993ad4164e935c7dbaa8b928cf94d440c1
tanabata000 <tanabata.kaguya@gmail.com>
2021-10-01 10:59:37 +0900

    Basic認証機能追加
    ユーザー名とPWはシェルの中に保存

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::Base
+  before_action :basic_auth
+
+  private
+
+  def basic_auth
+    # Basic認証を実装(ブロック名 username, password)
+    authenticate_or_request_with_http_basic do |username, password|
+    # 環境変数をRailsアプリケーション側で読み込む設定
+    username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]  
+    end
+  end
 end
