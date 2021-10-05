912e3ec2314f8f8186d32b2f78a5665b9a9a1679
tanabata000 <tanabata.kaguya@gmail.com>
2021-10-05 14:48:57 +0900

    ログイン状態の場合のみ、商品出品ページへ遷移
    itemモデル作成
    テーブル作成
    ログイン状態の場合のみ、商品出品ページへ遷移

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,41 @@
 class ItemsController < ApplicationController
   # 未ログインユーザーをログインページにリダイレクトする。例外アクション[:index, :show]
   # before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show, :search]
+
   def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
+  
+  def destroy
+  end
+
+  private
+  def item_params
+    params.require(:item).permit(:user, :image, :text).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_02_235007) do
+ActiveRecord::Schema.define(version: 2021_10_05_043001) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "item_name", null: false
+    t.text "item_info", null: false
+    t.integer "item_category_id", null: false
+    t.integer "item_sales_status_id", null: false
+    t.integer "item_shipping_fee_status_id", null: false
+    t.integer "item_prefecture_id", null: false
+    t.integer "item_scheduled_delivery_id", null: false
+    t.integer "item_price", null: false
+    t.string "first_name", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -30,4 +46,5 @@ ActiveRecord::Schema.define(version: 2021_10_02_235007) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
