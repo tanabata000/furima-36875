5ba1739a5b6e1946cf9f0cb5a98eace016cb7fa2
tanabata000 <tanabata.kaguya@gmail.com>
2021-10-14 17:32:32 +0900

    テストコードを日本語化に対応
    エラー文を日本語化することでincludeマッチャに設定したメッセージも変わるため修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -20,95 +20,95 @@ RSpec.describe User, type: :model do
         # itemレコードに紐づくuserテーブルを参照
         @item.user = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('User must exist')
+        expect(@item.errors.full_messages).to include('ユーザーを入力してください')
       end
       it '商品画像が存在しない' do
         # 画像が存在しない場合、値がnilとなるため。文字列ならば''で空欄を入力することで結果的にnilとなる。
         @item.image = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Image can't be blank")
+        expect(@item.errors.full_messages).to include("画像を入力してください")
       end
       it '商品名が存在しない' do
         @item.item_name = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item name can't be blank")
+        expect(@item.errors.full_messages).to include("商品名を入力してください")
       end
       it '商品の説明が存在しない' do
         @item.item_info = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item info can't be blank")
+        expect(@item.errors.full_messages).to include("商品の説明を入力してください")
       end
       # 商品の詳細
       it 'カテゴリーが未選択' do
         @item.item_category_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item category can't be blank")
+        expect(@item.errors.full_messages).to include("カテゴリーを入力してください")
       end
       it 'カテゴリーで「---」が選択されている' do
         @item.item_category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item category can't be blank")
+        expect(@item.errors.full_messages).to include("カテゴリーを入力してください")
       end
       it '商品名の状態が未選択' do
         @item.item_sales_status_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item sales status can't be blank")
+        expect(@item.errors.full_messages).to include("商品の状態を入力してください")
       end
       it '商品名の状態で「---」が選択されている' do
         @item.item_sales_status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item sales status can't be blank")
+        expect(@item.errors.full_messages).to include("商品の状態を入力してください")
       end
       it '配送料の負担が未選択' do
         @item.item_shipping_fee_status_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item shipping fee status can't be blank")
+        expect(@item.errors.full_messages).to include("配送料の負担を入力してください")
       end
       it '配送料の負担で「---」が選択されている' do
         @item.item_shipping_fee_status_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item shipping fee status can't be blank")
+        expect(@item.errors.full_messages).to include("配送料の負担を入力してください")
       end
       it '発送元の地域が未選択' do
         @item.item_prefecture_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item prefecture can't be blank")
+        expect(@item.errors.full_messages).to include("発送元の地域を入力してください")
       end
       it '発送元の地域で「---」が選択されている' do
         @item.item_prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item prefecture can't be blank")
+        expect(@item.errors.full_messages).to include("発送元の地域を入力してください")
       end
       it '発送までの日数が未選択' do
         @item.item_scheduled_delivery_id = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item scheduled delivery can't be blank")
+        expect(@item.errors.full_messages).to include("配送までの日数を入力してください")
       end
       it '発送までの日数で「---」が選択されている' do
         @item.item_scheduled_delivery_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Item scheduled delivery can't be blank")
+        expect(@item.errors.full_messages).to include("配送までの日数を入力してください")
       end
       # 販売価格
       it '販売価格が存在しない' do
         @item.item_price = nil
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item price is not included in the list', 'Item price is not a number')
+        expect(@item.errors.full_messages).to include("価格を入力してください", "価格は数値で入力してください", "価格は一覧にありません")
       end
       it '販売価格が300円未満' do
         @item.item_price = '200'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item price is not included in the list')
+        expect(@item.errors.full_messages).to include("価格は一覧にありません")
       end
       it '販売価格が9,999,999円を超える' do
         @item.item_price = '99999999'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item price is not included in the list')
+        expect(@item.errors.full_messages).to include("価格は一覧にありません")
       end
       it '販売価格に数字以外が入っている' do
         @item.item_price = 'aaa'
         @item.valid?
-        expect(@item.errors.full_messages).to include('Item price is not included in the list', 'Item price is not a number')
+        expect(@item.errors.full_messages).to include("価格は数値で入力してください", "価格は一覧にありません")
       end
     end
   end
--- spec/models/order_spec.rb
+++ spec/models/order_spec.rb
@@ -24,84 +24,84 @@ RSpec.describe Order, type: :model do
         # itemレコードに紐づくuserテーブルを参照
         @order.user_id = nil
         @order.valid?
-        expect(@order.errors.full_messages).to include("User can't be blank")
+        expect(@order.errors.full_messages).to include("ユーザーを入力してください")
       end
       it '商品情報が存在しない' do
         # itemレコードに紐づくuserテーブルを参照
         @order.item_id = nil
         @order.valid?
-        expect(@order.errors.full_messages).to include("Item can't be blank")
+        expect(@order.errors.full_messages).to include("商品情報を入力してください")
       end
       it 'トークン情報が存在しない' do
         # itemレコードに紐づくuserテーブルを参照
         @order.token = nil
         @order.valid?
-        expect(@order.errors.full_messages).to include("Token can't be blank")
+        expect(@order.errors.full_messages).to include("クレジットカード情報を入力してください")
       end
       it '郵便番号が存在しない' do
         @order.postal_code = nil
         @order.valid?
-        expect(@order.errors.full_messages).to include("Postal code can't be blank", 'Postal code is invalid')
+        expect(@order.errors.full_messages).to include("郵便番号を入力してください", "郵便番号は不正な値です")
       end
       it '郵便番号にハイフンがない' do
         @order.postal_code = '1111111'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Postal code is invalid')
+        expect(@order.errors.full_messages).to include("郵便番号は不正な値です")
       end
       it '郵便番号の桁が少ない' do
         @order.postal_code = '111-111'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Postal code is invalid')
+        expect(@order.errors.full_messages).to include("郵便番号は不正な値です")
       end
       it '郵便番号の桁が多い' do
         @order.postal_code = '1111-1111'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Postal code is invalid')
+        expect(@order.errors.full_messages).to include("郵便番号は不正な値です")
       end
       it '郵便番号が数字以外' do
         @order.postal_code = 'aaa'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Postal code is invalid')
+        expect(@order.errors.full_messages).to include("郵便番号は不正な値です")
       end
       it '都道府県が存在しない' do
         @order.item_prefecture_id = nil
         @order.valid?
-        expect(@order.errors.full_messages).to include("Item prefecture can't be blank")
+        expect(@order.errors.full_messages).to include("都道府県を入力してください", "都道府県を入力してください")
       end
       it '都道府県で「---」が選択されている' do
         @order.item_prefecture_id = 1
         @order.valid?
-        expect(@order.errors.full_messages).to include("Item prefecture can't be blank")
+        expect(@order.errors.full_messages).to include("都道府県を入力してください")
       end
       it '市区町村が存在しない' do
         @order.city = nil
         @order.valid?
-        expect(@order.errors.full_messages).to include("City can't be blank")
+        expect(@order.errors.full_messages).to include("市区町村を入力してください")
       end
       it '番地が存在しない' do
         @order.addresses = nil
         @order.valid?
-        expect(@order.errors.full_messages).to include("Addresses can't be blank")
+        expect(@order.errors.full_messages).to include("番地を入力してください")
       end
       it '電話番号が存在しない' do
         @order.phone_number = nil
         @order.valid?
-        expect(@order.errors.full_messages).to include("Phone number can't be blank", 'Phone number is invalid')
+        expect(@order.errors.full_messages).to include("電話番号を入力してください", "電話番号は不正な値です")
       end
       it '電話番号が数字以外' do
         @order.phone_number = 'Phone number is invalid'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Phone number is invalid')
+        expect(@order.errors.full_messages).to include("電話番号は不正な値です")
       end
       it '電話番号が9桁以下の場合' do
         @order.phone_number = '111111111'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Phone number is invalid')
+        expect(@order.errors.full_messages).to include("電話番号は不正な値です")
       end
       it '電話番号が12桁以上の場合' do
         @order.phone_number = '111111111111'
         @order.valid?
-        expect(@order.errors.full_messages).to include('Phone number is invalid')
+        expect(@order.errors.full_messages).to include("電話番号は不正な値です")
       end
     end
   end
